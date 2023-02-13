f035f917abde3feeaef25a04f712d7c375591d64
BAGOA <princeshtorm@gmail.com>
2023-02-13 20:31:43 +0300

    Чёт изменил)

---
+++ src/task7/Constants.java
@@ -0,0 +1,10 @@
+package task7;
+
+public final class Constants {
+    public static final String CREATE_ROBOT = "Был создан робот: ";
+    public static final String NAME = "Имя - ";
+    public static final String HEAD = ", Голова - ";
+    public static final String BODY = ", Тело - ";
+
+    private Constants(){}
+}
---
+++ src/task7/factory/Factory.java
@@ -0,0 +1,5 @@
+package task7.factory;
+
+public interface Factory<T> {
+    T[] create();
+}
