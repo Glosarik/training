9340f0a6fde3b325694df2c559274a5b5d003e30
BAGOA <princeshtorm@gmail.com>
2023-02-23 22:42:52 +0300

    Чёт изменил)

---
+++ src/task9/Constants.java
@@ -0,0 +1,29 @@
+package task9;
+
+public final class Constants {
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+    public static final int FOUR = 4;
+
+    public static final String OPTION = "\nВыберите опцию:";
+    public static final String OPTION_ONE = "1. Бросить исключение для проверки корректности имени пользователя";
+    public static final String OPTION_ONE_ONE = "Придумайте никнейм (используйте ТОЛЬКО анг.буквы):";
+    public static final String OPTION_ONE_TWO = "Добро пожаловать, ";
+    public static final String OPTION_TWO = "2. Бросить исключение для деления на 0";
+    public static final String OPTION_TWO_ONE = "Введите первое число:";
+    public static final String OPTION_TWO_TWO = "Введите первое число:";
+    public static final String OPTION_TWO_RES = "Результат: ";
+    public static final String OPTION_THREE = "3. Бросить исключение для проверки на отрицательное число";
+    public static final String OPTION_THREE_ONE = "Введите число: ";
+    public static final String OPTION_FOUR = "4. Выход";
+    public static final String FINALLY = "Выполнение блока finally";
+    public static final String EXCEPTION_ONE = "Your username contains other characters! Your username - ";
+    public static final String EXCEPTION_TWO = "Division by zero";
+    public static final String EXCEPTION_THREE_ONE = "Number can't be negative";
+    public static final String EXCEPTION_THREE_TWO = "Число положительное";
+
+    private Constants() {
+    }
+}
---
+++ src/task9/Test.java
@@ -0,0 +1,104 @@
+package task9;
+
+import java.util.Scanner;
+
+import static task9.Constants.*;
+
+public class Test {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void test() {
+        int option;
+        do {
+            printMenu();
+            option = scanner.nextInt();
+            switch (option) {
+                case ONE -> optionOne();
+                case TWO -> optionTwo();
+                case THREE -> optionThree();
+            }
+        } while (option != FOUR);
+    }
+
+    private static void printMenu() {
+        System.out.println(OPTION);
+        System.out.println(OPTION_ONE);
+        System.out.println(OPTION_TWO);
+        System.out.println(OPTION_THREE);
+        System.out.println(OPTION_FOUR);
+    }
+
+    public void optionOne() {
+        System.out.println(OPTION_ONE_ONE);
+        String username = scanner.next();
+        try {
+            if (!username.matches("[a-zA-Z]+")) {
+                throw new InvalidUsernameException(username);
+            } else {
+                System.out.println(OPTION_ONE_TWO + username);
+            }
+        } catch (InvalidUsernameException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println(FINALLY);
+        }
+    }
+
+    public void optionTwo() {
+        System.out.println(OPTION_TWO_ONE);
+        float a = scanner.nextInt();
+        System.out.println(OPTION_TWO_TWO);
+        float b = scanner.nextInt();
+        try {
+            if (b == 0) {
+                throw new DivisionByZeroException();
+            } else {
+                float c = a / b;
+                System.out.println(OPTION_TWO_RES + c);
+            }
+        } catch (DivisionByZeroException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println(FINALLY);
+        }
+    }
+
+    public void optionThree() {
+        System.out.println(OPTION_THREE_ONE);
+        int num = scanner.nextInt();
+        try {
+            throwCustomException3(num);
+        } catch (NegativeNumberException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println(FINALLY);
+        }
+    }
+
+    public static void throwCustomException3(int number) throws NegativeNumberException {
+        if (number < ZERO) {
+            throw new NegativeNumberException(EXCEPTION_THREE_ONE);
+        } else {
+            System.out.println(EXCEPTION_THREE_TWO);
+        }
+    }
+
+    static class InvalidUsernameException extends RuntimeException {
+        public InvalidUsernameException(String username) {
+            super(EXCEPTION_ONE + username);
+        }
+    }
+
+    static class DivisionByZeroException extends RuntimeException {
+        public DivisionByZeroException() {
+            super(EXCEPTION_TWO);
+        }
+    }
+
+    static class NegativeNumberException extends Exception {
+        public NegativeNumberException(String message) {
+            super(message);
+        }
+    }
+}
